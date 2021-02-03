7d8d8b3c72a08dea0a68cdb277ef7b8d8b4e80ce
Daniel Bermudes <dbermudes@berklee.edu>
2021-02-03 10:54:21 -0500

    Class follow alongs

---
+++ Variables.c
@@ -0,0 +1,14 @@
+//For printing
+#include <stdio.h>
+
+int main(){
+    //Declaring and initializing Variables
+    int number = 123;
+    double pi = 3.1416;
+    float z = 40.5f
+    char c = 'A';
+
+
+    printf("number: %d, pi: %lf, z: %f, c: %c\n", number, pi, z, c);
+    return 0;
+}
